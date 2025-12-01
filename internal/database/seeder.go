d8925f5a6c410972f5542f2fb1829543226702f1
riparuk <faruqirifa@gmail.com>
2025-12-01 13:15:44 +0700

    fix: user seeder

--- internal/database/seeder.go
+++ internal/database/seeder.go
@@ -3,14 +3,21 @@ package database
 import (
 	"log"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/riparuk/go-gin-starter-simple/internal/model"
 )
 
 func Seed() {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
+	if err != nil {
+		log.Printf("seeding error: %v", err)
+		return
+	}
 	users := []model.User{
-		{Name: "Alice"},
-		{Name: "Bob"},
-		{Name: "Charlie"},
+		{Name: "Alice", Email: "alice@example.com", Password: string(hashedPassword)},
+		{Name: "Bob", Email: "bob@example.com", Password: string(hashedPassword)},
+		{Name: "Charlie", Email: "charlie@example.com", Password: string(hashedPassword)},
 	}
 
 	for _, user := range users {
